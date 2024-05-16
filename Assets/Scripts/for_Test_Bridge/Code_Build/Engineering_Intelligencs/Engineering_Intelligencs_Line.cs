d4a8dc6306bb36ac6568fa3bfb9041b4e675d158
ANTON2002ARS <101361041+ANTON2002ARS@users.noreply.github.com>
2024-05-16 20:21:07 +0400

    new text on button and flag

--- Assets/Scripts/for_Test_Bridge/Code_Build/Engineering_Intelligencs/Engineering_Intelligencs_Line.cs
+++ Assets/Scripts/for_Test_Bridge/Code_Build/Engineering_Intelligencs/Engineering_Intelligencs_Line.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class Engineering_Intelligencs_Line : MonoBehaviour
 {
+    [SerializeField] private bool is_review;
     public bool is_set { private set; get; }
     [SerializeField] private Mistake_build mistake;
     public Mistake_build Get_Mistake => mistake;
     private Animator animator_line;
-    private void Start() => animator_line = GetComponent<Animator>(); 
+
+    private void Start()
+    {
+        animator_line = GetComponent<Animator>();
+        if(is_review)
+            animator_line.SetTrigger("is_pull_up");
+    }
     
     public void On_Click()
     {
+        if (is_review)
+            return;        
         is_set = true;
         animator_line.SetTrigger("is_pull_up");  
     }        
